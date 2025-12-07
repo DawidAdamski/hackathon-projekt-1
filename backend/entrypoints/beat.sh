673f1c4e6d753707def501ffceb20febb66abc59
Dawid Adamski <dawid458@gmail.com>
2025-12-07 09:21:12 +0100

    readme

--- backend/entrypoints/beat.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-celery -A app.celery_app.celery_app beat -l info
--- backend/entrypoints/worker.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-celery -A app.celery_app.celery_app worker -l info -Q fast,fail,slow,celery
